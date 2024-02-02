976a53b07eca37aff1ae73af0ce5e202691e24dc
MikeMwita <mwitamike8@gmail.com>
2024-02-02 15:11:51 +0300

    Binary search

--- binary_search/main.go
+++ binary_search/main.go
@@ -1,5 +1,50 @@
 package binary_search
 
+import "errors"
+
+type binTree interface {
+	Insert(int)
+}
+type TreeNode struct {
+	val   int
+	left  *TreeNode
+	right *TreeNode
+}
+
+func (t *TreeNode) Insert(value int) error {
+	if t == nil {
+		return errors.New("Tree is nil")
+	}
+
+	if t.val == value {
+		return errors.New("This node value already exists")
+	}
+
+	if t.val > value {
+		if t.left == nil {
+			t.left = &TreeNode{val: value}
+			return nil
+		}
+		return t.left.Insert(value)
+	}
+
+	if t.val < value {
+		if t.right == nil {
+			t.right = &TreeNode{val: value}
+			return nil
+		}
+		return t.right.Insert(value)
+	}
+	return nil
+}
+
+func (t *TreeNode) FindMin() int {
+	if t.left == nil {
+		return t.val
+	}
+	return t.left.FindMin()
+}
+
 func binSearch(arr []int, target int) int {
 	low := 0
 	high := len(arr) - 1
